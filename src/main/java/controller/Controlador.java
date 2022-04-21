263d4cc16efc140105730e5fe7fddd0151e1885c
Kotyx <72136713+Kotyx@users.noreply.github.com>
2022-04-21 20:07:44 +0200

    Add files via upload

--- src/main/java/controller/Controlador.java
+++ src/main/java/controller/Controlador.java
@@ -8,16 +8,17 @@ import com.opencsv.exceptions.CsvValidationException;
 import interfaces.IVideojuegoService;
 import services.VideojuegoService;
 
+
 public class Controlador {
 	public static void main(String[] args) {
-
-		IVideojuegoService vjservice = new VideojuegoService();
+		
+		IVideojuegoService vjservice=new VideojuegoService();
 		Scanner sc = new Scanner(System.in);
-
-		// MENU DEL CATALOGO DE VIDEOJUEGOS LUCASTEAM
+		
+		//MENU DEL CATALOGO DE VIDEOJUEGOS LUCASTEAM
 		System.out.println("PLATAFORMA LUCASTEAM: MENU PRINCIPAL");
-		boolean seguir = true;
-		while (seguir) {
+		boolean seguir=true;
+		while(seguir) {
 			System.out.println("***************************************");
 			System.out.println("1--A�ADIR UN VIDEOJUEGO AL CATALOGO");
 			System.out.println("2--LISTA DE VIDEOJUEGOS GENERAL");
@@ -31,52 +32,94 @@ public class Controlador {
 			System.out.println("10-VER LA LISTA DE EDITORES");
 			System.out.println("0--SALIR");
 			System.out.println("***************************************");
-			int opcion = sc.nextInt();
-			switch (opcion) {
-			case 1:
-				try {
-					vjservice.darDeAltaVideojuego();
-				} catch (Exception e) {
-					System.out.println("No se ha podido a�adir el videojuego.");
-				}
+			int opcion=sc.nextInt();
+			switch(opcion)
+			{
+				case 1 :
+					try {
+						vjservice.darDeAltaVideojuego();
+					} catch(Exception e) {
+						System.out.println("No se ha podido a�adir el videojuego.");
+					}
+					break; 
+				case 2 :
+					try {
+						vjservice.listado_videojuegos();
+					} catch (CsvValidationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break; 
+				case 3 :
+					try {
+						vjservice.listado_videojuegos_plataforma();
+					} catch (CsvValidationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break; 
+				case 4:
+					try {
+						vjservice.darDeBajaVideojuego();
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+				case 5:
+					System.out.println("Opcion en mantenimiento...");
+					break;
+				case 6:
+					try {
+						vjservice.listado_videojuegos_sigloxx();
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+				case 7:
+					try {
+						System.out.println("Dime un genero a buscar: ");
+						String genero=sc.next();
+						vjservice.listado_videojuegos_genero(genero);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+				case 8:
+					try {
+						vjservice.listado_videojuegos_nintendo();
+					} catch (CsvValidationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+				case 9 :
+					try {
+						vjservice.listado_videojuegos_anyo_par();
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+				case 10:
+					System.out.println("Opcion en mantenimiento...");
+					break;
+				case 0:
+					seguir=false;
 				break;
-			case 2:
-				vjservice.listado_videojuegos();
-				break;
-			case 3:
-				System.out.println("Opcion en mantenimiento...");
-				break;
-			case 4:
-				try {
-					vjservice.darDeBajaVideojuego();
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				break;
-			case 5:
-				System.out.println("Opcion en mantenimiento...");
-				break;
-			case 6:
-				System.out.println("Opcion en mantenimiento...");
-				break;
-			case 7:
-				System.out.println("Opcion en mantenimiento...");
-				break;
-			case 8:
-				System.out.println("Opcion en mantenimiento...");
-				break;
-			case 9:
-				System.out.println("Opcion en mantenimiento...");
-				break;
-			case 10:
-				System.out.println("Opcion en mantenimiento...");
-				break;
-			case 0:
-				seguir = false;
-				break;
-			default:
-				System.out.println("Seleccione una opcion valida");
+				default :
+					System.out.println("Seleccione una opcion valida");
 			}
 		}
 		System.out.println("***************************************");
