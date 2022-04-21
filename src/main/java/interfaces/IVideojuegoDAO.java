09e5c2386e66b9b2b0c7e24b5b72ef05d1806430
GuillermoFrances <102316583+GuillermoFrances@users.noreply.github.com>
2022-04-21 11:43:32 +0200

    Add files via upload

--- src/main/java/interfaces/IVideojuegoDAO.java
+++ src/main/java/interfaces/IVideojuegoDAO.java
@@ -1,12 +1,17 @@
 package interfaces;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.opencsv.exceptions.CsvValidationException;
+
 import model.Videojuego;
 
 public interface IVideojuegoDAO {
 	
 	// contiene los metodos del DAO
-	
-	// public void addVideojuego(Videojuego videojuego);
+
 	public void addVideojuego(Videojuego videojuego);
+	public ArrayList<String[]> listado() throws CsvValidationException, IOException;
 
 }
--- src/main/java/interfaces/IVideojuegoService.java
+++ src/main/java/interfaces/IVideojuegoService.java
@@ -3,11 +3,13 @@ package interfaces;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import com.opencsv.exceptions.CsvValidationException;
+
 import model.Videojuego;
 
 public interface IVideojuegoService {
 	
 	public void darDeAltaVideojuego() throws IOException;
-	public ArrayList<Videojuego> listado_videojuegos(); 
+	public void listado_videojuegos() throws CsvValidationException, IOException; 
 
 }
